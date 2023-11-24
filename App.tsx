27cb17163dc2a5e59af5fd308c1e1176e199d02c
Isaac Ojo <hojoisaac@gmail.com>
2023-11-24 05:28:14 +0100

    switched to react native image for perfromance

--- App.tsx
+++ App.tsx
@@ -5,34 +5,11 @@ import { StyleSheet, Text, View } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import { NavigationContainer } from "@react-navigation/native";
 import { MainApp } from "@/navigation/Main";
-import { ImageBackground, ImageSource } from "expo-image";
+import { ImageSource, ImageBackground } from "expo-image";
 import { Asset, useAssets } from "expo-asset";
 import { useIconStore, useMusicStore } from "@/zustand/store";
 SplashScreen.preventAutoHideAsync();
 export default function App() {
-  const { iconAssets, setIconAssets } = useIconStore();
-  const [assets, error] = useAssets([require("@/assets/images/bg.webp")]);
-  const [assetsIcon, errorIcon] = useAssets([
-    require("@/assets/symbols/1.svg"),
-    require("@/assets/symbols/2.svg"),
-    require("@/assets/symbols/3.svg"),
-    require("@/assets/symbols/4.svg"),
-    require("@/assets/symbols/5.svg"),
-    require("@/assets/symbols/6.svg"),
-    require("@/assets/symbols/8.svg"),
-    require("@/assets/symbols/10.svg"),
-    require("@/assets/symbols/11.svg"),
-    require("@/assets/symbols/12.svg"),
-    require("@/assets/symbols/13.svg"),
-    require("@/assets/symbols/14.svg"),
-    require("@/assets/symbols/15.svg"),
-    require("@/assets/symbols/17.svg"),
-    require("@/assets/symbols/18.svg"),
-    require("@/assets/symbols/19.svg"),
-    require("@/assets/symbols/20.svg"),
-    require("@/assets/symbols/23.svg"),
-  ]);
-  
   const [fontsLoaded, fontError] = useFonts({
     breathFire: require("@/assets/fonts/BreatheFire.otf"),
     luckiestGuy: require("@/assets/fonts/LuckiestGuy-Regular.ttf"),
@@ -44,13 +21,12 @@ export default function App() {
     latoThin: require("@/assets/fonts/Lato-Thin.ttf"),
   });
   const onLoaded = useCallback(async () => {
-    if ((fontsLoaded && assets && assetsIcon) || fontError) {
-      assetsIcon && setIconAssets(assetsIcon);
+    if (fontsLoaded || fontError) {
       setTimeout(async () => {
         await SplashScreen.hideAsync();
       }, 1000);
     }
-  }, [fontsLoaded, fontError, assets, assetsIcon]);
+  }, [fontsLoaded, fontError]);
 
   if (!fontsLoaded && !fontError) {
     return null;
@@ -59,11 +35,13 @@ export default function App() {
   return (
     <NavigationContainer onReady={onLoaded}>
       <StatusBar style="light" />
-      {assets && (
-        <ImageBackground style={{ flex: 1 }} source={assets[0] as ImageSource}>
-          <MainApp />
-        </ImageBackground>
-      )}
+
+      <ImageBackground
+        style={{ flex: 1 }}
+        source={require("@/assets/images/bg.webp")}
+      >
+        <MainApp />
+      </ImageBackground>
     </NavigationContainer>
   );
 }
